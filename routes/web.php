5179bafdd06d51e78b914d8e3faa16294fa97b32
Yuya Kasai <moyoyomiyazawa@gmail.com>
2020-07-24 21:24:45 +0900

    記事更新・削除機能実装 / 記事詳細画面追加 / 認可・ポリシー追加

--- routes/web.php
+++ routes/web.php
@@ -2,4 +2,6 @@
 
 Auth::routes();
 Route::get('/', 'ArticleController@index')->name('articles.index');
-Route::resource('/articles', 'ArticleController')->except(['index'])->middleware('auth');
+Route::resource('/articles', 'ArticleController')->except(['index', 'show'])->middleware('auth');
+// 記事詳細画面はログインユーザーじゃなくても見れるようにしたいので、部分的にauthミドルウェアがついていないルートを定義する
+Route::resource('/articles', 'ArticleController')->only(['show']);
