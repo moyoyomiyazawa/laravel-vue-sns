7be001a4e747b6c55d5ade50e1220c2815165b75
Yuya Kasai <moyoyomiyazawa@gmail.com>
2020-08-22 12:13:41 +0900

    ユーザーページの記事一覧実装

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,13 +7,29 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    // ユーザーページ
+    // ユーザーページ（ユーザーが投稿した記事一覧画面）
     public function show(string $name)
     {
         $user = User::where('name', $name)->first();
 
+        $articles = $user->articles->sortByDesc('created_at');
+
         return view('users.show', [
             'user' => $user,
+            'articles' => $articles,
+        ]);
+    }
+
+    // いいねした記事一覧画面
+    public function likes(string $name)
+    {
+        $user = User::where('name', $name)->first();
+
+        $articles = $user->likes->sortByDesc('created_at');
+
+        return view('users.likes', [
+            'user' => $user,
+            'articles' => $articles,
         ]);
     }
 
